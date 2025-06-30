f328bfc5f53554c04afd67f60cb5743ae4dc1ca7
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2025-06-30 12:29:09 +0530

    feat: initialize space_portfolio project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Included Vite logo SVG in public directory.
    - Created App component with routing setup for Home and NotFound pages.
    - Added React logo SVG in assets directory.
    - Created initial CSS files for global styles and Tailwind CSS integration.
    - Implemented main entry point (main.jsx) for rendering the App component.
    - Developed Home and NotFound pages with basic structure.
    - Configured Vite with React and Tailwind CSS plugins

---
+++ space_portfolio/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen '>
+        {/* THeme toggle */}
+
+
+        {/* Background Effects */}
+
+
+        {/* navbar */}
+
+
+        {/* main content  */}
+
+
+
+        {/* footer */}
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Home
