d37a342f2cc3114a746c2cf61dc3c3b6ffde9626
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2025-07-07 12:57:37 +0530

    update

--- space_portfolio/src/pages/Home.jsx
+++ space_portfolio/src/pages/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ThemeToggle from '../components/ThemeToggle'
 import StarBackground from '../components/StarBackground'
 import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import AboutMe from '../components/AboutMe'
+import SkillSection from '../components/SkillSection'
+import ProjectSection from '../components/ProjectSection'
+import ContactSection from '../components/COntactSection'
+
 
 const Home = () => {
   return (
@@ -23,6 +29,14 @@ const Home = () => {
 
 
         {/* main content  */}
+        <main>
+          <HeroSection></HeroSection>
+          <AboutMe></AboutMe>
+          <SkillSection></SkillSection>
+          <ProjectSection></ProjectSection>
+          <ContactSection></ContactSection>
+
+        </main>
 
 
 
